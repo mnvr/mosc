40cec763be02bf08e1588cb0772c99a35b2eebbb
Manav <manav@mrmr.io>
2023-07-29 09:49:36 +0530

    Try adding hosc

--- src/MOSC.hs
+++ src/MOSC.hs
@@ -4,6 +4,8 @@ module MOSC where
 
 import Control.Concurrent
 import Data.Time
+import Sound.Osc
+import qualified Sound.Osc as Sound.Osc.Fd
 
 version :: IO String
 version = show <$> Data.Time.getCurrentTime
@@ -11,5 +13,13 @@ version = show <$> Data.Time.getCurrentTime
 foreverNow :: Int -> IO ()
 foreverNow t = do
   version >>= putStrLn
+  -- putStrLn versionMessage
   threadDelay (t * 1_000_000)
   foreverNow t
+
+versionMessage :: Message
+versionMessage = message "/version" []
+
+-- transport = withTransport u1 (\fd -> Sound.Osc.Fd.sendMessage fd (Sound.Osc.Fd.Packet_Message "/n" []))
+--   where
+--     u1 = openUdp "127.0.0.1" 57300
