ca8a0a12870ae7a2b9ab14d54272263af4091d7a
Manav <manav@mrmr.io>
2023-07-29 13:03:13 +0530

    AddrInfo

--- src/MOSC.hs
+++ src/MOSC.hs
@@ -3,6 +3,7 @@ module MOSC where
 import Control.Concurrent
 import Data.Maybe
 import Data.Time
+import Network.Socket
 import Sound.Osc.Fd
 
 printVersion :: IO ()
@@ -21,10 +22,17 @@ toString (Just ms) = show ms
 toString Nothing = "ERR"
 
 requestVersion :: IO ()
-requestVersion = udpOut >>= (`sendMessage` versionMessage)
+-- requestVersion = udpListener >>= (\u -> sendTo u (Packet_Message
+-- versionMessage) Network.Socket.addr)
+requestVersion = putStrLn "test"
+
+-- | Perform DNS resolution to convert a host:port combination into an AddrInfo
+resolve :: String -> Int -> IO (Maybe AddrInfo)
+resolve host port =
+  listToMaybe <$> getAddrInfo (Just defaultHints) (Just host) (Just (show port))
 
 versionMessage :: Message
-versionMessage = Message "/version" []
+versionMessage = Message "/version" [string ""]
 
 udpOut :: IO Udp
 udpOut = openUdp "127.0.0.1" 57110
@@ -32,4 +40,10 @@ udpOut = openUdp "127.0.0.1" 57110
 udpIn :: IO Udp
 udpIn = openUdp "127.0.0.1" 44440 -- 0 allocates a random port?
 
+-- | A UDP socket
+--
+-- We can both send and receive using the same socket. When communicating with
+-- SuperCollider, it is imperative that we send on the same socket that we're
+-- listening on since SuperCollider sends replies there.
+udpListener :: IO Udp
 udpListener = udpServer "127.0.0.1" 44440
