64819a226e76000e9a76331ca87a0cbd1bd9de10
Manav <manav@mrmr.io>
2023-07-29 10:42:17 +0530

    Cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -6,13 +6,4 @@ import System.Environment
 import Text.Read
 
 main :: IO ()
-main = do
-  d <- parseDuration <$> getArgs
-  printMessage d
-  foreverNow d
-
-parseDuration :: [String] -> Int
-parseDuration args = fromMaybe 3 $ listToMaybe args >>= readMaybe
-
-printMessage :: Int -> IO ()
-printMessage d = putStrLn ("Every " ++ show d ++ " seconds I'll remind you, that I exist. I am time.")
+main = printVersion
--- src/MOSC.hs
+++ src/MOSC.hs
@@ -4,23 +4,11 @@ import Control.Concurrent
 import Data.Time
 import Sound.Osc.Fd
 
-version :: IO String
-version = show <$> Data.Time.getCurrentTime
-
-foreverNow :: Int -> IO ()
-foreverNow t = do
-  (Sound.Osc.Fd.openUdp "127.0.0.1" 6012) >>= (\r -> Sound.Osc.Fd.sendMessage r $ (Message "/version" []))
-
--- version >>= putStrLn
--- (Packet.message "/n" [])
--- Sound.Osc.Fd.sendMessage udpOut versionMessage
+printVersion :: IO ()
+printVersion = udpOut >>= (`sendMessage` versionMessage)
 
 versionMessage :: Message
 versionMessage = Message "/version" []
 
--- udpOut :: IO Udp
+udpOut :: IO Udp
 udpOut = openUdp "127.0.0.1" 6012
-
--- transport = withTransport u1 (\fd -> Sound.Osc.Fd.sendMessage fd (Sound.Osc.Fd.Packet_Message "/n" []))
---   where
---     u1 = openUdp "127.0.0.1" 57300
