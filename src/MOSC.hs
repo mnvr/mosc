a45b81ac08f025fb4af334da39fecce29438c81c
Manav <manav@mrmr.io>
2023-07-29 10:38:20 +0530

    Send UDP message

--- src/MOSC.hs
+++ src/MOSC.hs
@@ -1,24 +1,25 @@
-{-# LANGUAGE NumericUnderscores #-}
-
 module MOSC where
 
 import Control.Concurrent
 import Data.Time
-import Sound.Osc
-import qualified Sound.Osc as Sound.Osc.Fd
+import Sound.Osc.Fd
 
 version :: IO String
 version = show <$> Data.Time.getCurrentTime
 
 foreverNow :: Int -> IO ()
 foreverNow t = do
-  version >>= putStrLn
-  -- putStrLn versionMessage
-  threadDelay (t * 1_000_000)
-  foreverNow t
+  (Sound.Osc.Fd.openUdp "127.0.0.1" 6012) >>= (\r -> Sound.Osc.Fd.sendMessage r $ (Message "/version" []))
+
+-- version >>= putStrLn
+-- (Packet.message "/n" [])
+-- Sound.Osc.Fd.sendMessage udpOut versionMessage
 
 versionMessage :: Message
-versionMessage = message "/version" []
+versionMessage = Message "/version" []
+
+-- udpOut :: IO Udp
+udpOut = openUdp "127.0.0.1" 6012
 
 -- transport = withTransport u1 (\fd -> Sound.Osc.Fd.sendMessage fd (Sound.Osc.Fd.Packet_Message "/n" []))
 --   where
