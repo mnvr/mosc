da5ce77d012be2825d167ae44e85fcc20c6e4094
Manav <manav@mrmr.io>
2023-07-29 12:16:37 +0530

    udpServer

--- src/MOSC.hs
+++ src/MOSC.hs
@@ -1,14 +1,35 @@
 module MOSC where
 
 import Control.Concurrent
+import Data.Maybe
 import Data.Time
 import Sound.Osc.Fd
 
 printVersion :: IO ()
-printVersion = udpOut >>= (`sendMessage` versionMessage)
+printVersion = do
+  forkIO requestVersion
+  receiveVersion >>= putStrLn
+
+receiveVersion :: IO String
+receiveVersion = do
+  u <- udpListener
+  m <- recvMessage u
+  return (toString m)
+
+toString :: Maybe Message -> String
+toString (Just ms) = show ms
+toString Nothing = "ERR"
+
+requestVersion :: IO ()
+requestVersion = udpOut >>= (`sendMessage` versionMessage)
 
 versionMessage :: Message
 versionMessage = Message "/version" []
 
 udpOut :: IO Udp
-udpOut = openUdp "127.0.0.1" 6012
+udpOut = openUdp "127.0.0.1" 57110
+
+udpIn :: IO Udp
+udpIn = openUdp "127.0.0.1" 44440 -- 0 allocates a random port?
+
+udpListener = udpServer "127.0.0.1" 44440
