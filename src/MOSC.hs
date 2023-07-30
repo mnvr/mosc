817ccb19a9ab2d46d98b984224f080ce89263972
Manav <manav@mrmr.io>
2023-07-30 07:41:10 +0530

    Some exception

--- src/MOSC.hs
+++ src/MOSC.hs
@@ -1,20 +1,25 @@
 module MOSC where
 
 import Control.Concurrent
+import Control.Exception
+import Data.Data
 import Data.Maybe
 import Data.Time
 import Network.Socket
 import Sound.Osc.Fd
 
+data MOSCException = HostNameResolutionFailed {host :: String, port :: Int}
+  deriving (Show, Typeable)
+
+instance Exception MOSCException
+
 printVersion :: IO ()
 printVersion = do
-  forkIO requestVersion
-  receiveVersion >>= putStrLn
+  sock <- udpListener; forkIO requestVersion; receiveVersion sock >>= putStrLn
 
-receiveVersion :: IO String
-receiveVersion = do
-  u <- udpListener
-  m <- recvMessage u
+receiveVersion :: Udp -> IO String
+receiveVersion sock = do
+  m <- recvMessage sock
   return (toString m)
 
 toString :: Maybe Message -> String
@@ -26,7 +31,23 @@ requestVersion :: IO ()
 -- versionMessage) Network.Socket.addr)
 requestVersion = putStrLn "test"
 
--- | Perform DNS resolution to convert a host:port combination into an AddrInfo
+-- requestVersion sock = sendTo sock (Packet_Message versionMessage) sa
+-- where
+-- sa = resolve
+
+-- | Return an 'AddrInfo' representing 'targetHost':'targetPort'
+resolveTarget :: IO AddrInfo
+resolveTarget = resolveTarget_ >>= toE
+
+resolveTarget_ = resolve targetHost targetPort
+
+toE :: Maybe AddrInfo -> IO AddrInfo
+toE mAddr = case mAddr of
+  Nothing -> throw (HostNameResolutionFailed {host = targetHost, port = targetPort})
+  (Just addr) -> return addr
+
+-- | Perform DNS resolution to convert a host:port combination into an
+-- 'AddrInfo'
 resolve :: String -> Int -> IO (Maybe AddrInfo)
 resolve host port =
   listToMaybe <$> getAddrInfo (Just defaultHints) (Just host) (Just (show port))
@@ -46,4 +67,12 @@ udpIn = openUdp "127.0.0.1" 44440 -- 0 allocates a random port?
 -- SuperCollider, it is imperative that we send on the same socket that we're
 -- listening on since SuperCollider sends replies there.
 udpListener :: IO Udp
-udpListener = udpServer "127.0.0.1" 44440
+udpListener = udpServer targetHost targetPort
+
+-- | The hostname where we should send OSC messages
+targetHost :: String
+targetHost = "127.0.0.1"
+
+-- | The port on the 'targetHost' where we should send OSC messages
+targetPort :: Int
+targetPort = 44440
